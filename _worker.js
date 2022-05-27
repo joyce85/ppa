6a4275e6b7d489044e559def60232d9a0f221188
joyce85 <105416772+joyce85@users.noreply.github.com>
2022-05-27 12:44:27 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    if (url.pathname.startsWith('/2d1d9c50-c9ad-4c7a-b3a1-9fcd38f62b5a-vmess')) {
+    if (url.pathname.startsWith('/')) {
       url.hostname = 'aew3tdf.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
